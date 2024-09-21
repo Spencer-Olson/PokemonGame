187ab42c2bedae4d7fc865ac6766576e0412dd6b
Spencer Olson <46606268+Spencer-Olson@users.noreply.github.com>
2024-09-20 20:17:22 -0500

    reupload character folder

---
+++ Assets/Scenes/Scripts/Character/NPCController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour, Interactable
+{
+    [SerializeField] Dialogue dialogue;
+    public void Interact()
+    {
+        StartCoroutine(DialogueManager.Instance.ShowDialogue(dialogue));
+        //Debug.Log("Interacting with NPC");
+    }
+}
