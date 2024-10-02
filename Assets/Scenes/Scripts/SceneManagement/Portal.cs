ab11edc00603bb7c54937a30d856526401027e14
Spencer Olson <spencer51902@gmail.com>
2024-10-01 19:20:56 -0500

    Added Fading effect to scene switching

--- Assets/Scenes/Scripts/SceneManagement/Portal.cs
+++ Assets/Scenes/Scripts/SceneManagement/Portal.cs
@@ -10,10 +10,18 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
     [SerializeField] DestinationIdentifier destinationPortal;
     [SerializeField] Transform spawnPoint;
     PlayerController player;
+    
+
     public void OnPlayerTriggered(PlayerController player)
     {
         this.player = player;
         StartCoroutine(SwitchScene());
+    }
+
+    Fader fader;
+    private void Start()
+    {
+       fader = FindObjectOfType<Fader>();
     }
 
     IEnumerator SwitchScene()
@@ -22,11 +30,13 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
         DontDestroyOnLoad(gameObject);
 
         GameController.Instance.PauseGame(true);
+        yield return fader.FadeIn(.5f);
 
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
         var destPortal = FindObjectsOfType<Portal>().First(x => x != this && x.destinationPortal == this.destinationPortal);
         player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
+        yield return fader.FadeOut(.5f);
 
         GameController.Instance.PauseGame(false);
 
