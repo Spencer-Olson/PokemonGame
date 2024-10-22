026138ac7eac4b01844546166dbfa37069aceaa3
Spencer Olson <46606268+Spencer-Olson@users.noreply.github.com>
2024-10-21 20:55:09 -0500

    Add files via upload

--- Assets/Scenes/Scripts/Core/EssentialObjectsSpawner.cs
+++ Assets/Scenes/Scripts/Core/EssentialObjectsSpawner.cs
@@ -11,7 +11,15 @@ public class EssentialObjectsSpawner : MonoBehaviour
         var existingObjects = FindObjectsOfType<EssentialObjects>();
         if(existingObjects.Length == 0)
         {
-            Instantiate(essentialObjects, new Vector3(0, 0, 0), Quaternion.identity);
+            //if grid, spawn at center
+            var spawnPos = new Vector3(0, 0, 0);
+            var grid = FindObjectOfType<Grid>();
+            if (grid != null)
+            {
+                spawnPos = grid.transform.position;
+            }
+
+            Instantiate(essentialObjects, spawnPos, Quaternion.identity);
         }
     }
 }
